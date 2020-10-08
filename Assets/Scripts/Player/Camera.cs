13a8227af24816792a8db14ac9059ed6c67cbb3e
Robert Kalman <RKalman@graphisoft.com>
2020-10-08 21:42:39 +0200

    Kicked it off
    Added Neon Light
    Added Physics
    Added Control

---
+++ Assets/Scripts/Player/Camera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [Header("Mouse Sensitivity")]
+    [Tooltip("Default value is 100")]
+    public float mouseSensitivity = 100f;
+
+    [Header("The player Body which is rotated on X movement")]
+    [Tooltip("Link the player object here")]
+    public Transform playerBody;
+
+    float yRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        yRotation -= mouseY;
+        yRotation = Mathf.Clamp(yRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(yRotation, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float gravity;
+    public float speed = 12f;
+    public float jumpHeight = 10;
+
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+
+
+    private Vector3 velocity;
+    private bool isGrounded;
+
+
+    void Update()
+    {
+        GroundCheck();
+
+        HandleJump();
+
+        Movement();
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 moveVector = transform.right * x + transform.forward * z;
+
+        controller.Move(moveVector * speed * Time.deltaTime);
+    }
+
+    private void GroundCheck()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            //looks better if the character is forced to the ground
+            velocity.y = -2f;
+        }
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                // https://www.thehoopsgeek.com/the-physics-of-the-vertical-jump/
+                velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+            }
+        }
+    }
+}
